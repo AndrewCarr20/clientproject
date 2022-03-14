c4b8caca8977078a55112672bb90400d4f443b1e
AndrewCarr20 <andrew.carranza70@gmail.com>
2022-03-13 23:44:23 -0500

    Add files via upload

---
+++ indexHistoryScript.js
@@ -0,0 +1,39 @@
+
+
+// history script table...
+// dead ass need to create a function that makes table bigger when i call it with jquery or some shit...
+const form = document.getElementById("table");
+const row1 = document.getElementById("row1");
+const submit = document.getElementById() // need to get from submit button
+submit.addEventListener( 'submit', (e)=> {
+  e.defaultPrevented();
+
+
+
+
+})
+
+function createNewTable(){
+  columns: [
+            {},
+            {},
+            {},
+            {},
+            {}
+
+
+
+
+  ]
+  
+
+
+}
+
+
+
+
+
+
+
+
